f09e20410ab7bc72aaf33706e2e1295fa1a5694c
Kwun-Lok Ng <loki.nkl@gmail.com>
2026-03-13 11:37:22 +0200

    Update SHA256 hashes from v0.1.0 releases
    Both i18n-convert and i18n-pseudo now have real SHA256 hashes
    from GitHub Release binaries. brew install will work.

--- Formula/i18n-convert.rb
+++ Formula/i18n-convert.rb
@@ -7,20 +7,20 @@ class I18nConvert < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/i18n-agent/i18n-convert/releases/download/v#{version}/i18n-convert-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      sha256 "f194cd57f3ca088eb6ed2e81721850c03a45ee7b060359d4b6c28ba68a62d551"
     else
       url "https://github.com/i18n-agent/i18n-convert/releases/download/v#{version}/i18n-convert-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_X86_64"
+      sha256 "c90980992ef9bd5834dc84823fa90557f10e8f9111867a729f9e37fdadf91266"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/i18n-agent/i18n-convert/releases/download/v#{version}/i18n-convert-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "fea40bb77fce2bf303b7b5d654c651e3e36c0e532cb835eef556a9fadf818fdd"
     else
       url "https://github.com/i18n-agent/i18n-convert/releases/download/v#{version}/i18n-convert-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X86_64"
+      sha256 "05fd715f868fbf07cd381ab339c098e03652d281fa52be8ed98e0230fb64029d"
     end
   end
 
--- Formula/i18n-pseudo.rb
+++ Formula/i18n-pseudo.rb
@@ -7,20 +7,20 @@ class I18nPseudo < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/i18n-agent/i18n-pseudo/releases/download/v#{version}/i18n-pseudo-aarch64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_ARM64"
+      sha256 "177ed0d223dbc6ab39d7aeb677eda825e271100550d90f426aaee8650e74dc4a"
     else
       url "https://github.com/i18n-agent/i18n-pseudo/releases/download/v#{version}/i18n-pseudo-x86_64-apple-darwin.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_MACOS_X86_64"
+      sha256 "61cb33b4c00bb6c236127aae0577688c6cf3d62f762fcf223688039485ca2dc3"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/i18n-agent/i18n-pseudo/releases/download/v#{version}/i18n-pseudo-aarch64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_ARM64"
+      sha256 "babad643f1ea1231e7213d877470ef5bd4736a1cf711ed48eefccad73b2dbcc7"
     else
       url "https://github.com/i18n-agent/i18n-pseudo/releases/download/v#{version}/i18n-pseudo-x86_64-unknown-linux-gnu.tar.gz"
-      sha256 "PLACEHOLDER_SHA256_LINUX_X86_64"
+      sha256 "fd00bcf5b1fafbcc03e5bd3b757592fc03004bc2dd93ae5af19713e0835c942d"
     end
   end
 
